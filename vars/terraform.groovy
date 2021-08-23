aa9e3eead93a453dc368eea0ac2faf4ad49ae009
venu <venugopal.k1994@gmail.com>
2021-08-23 17:57:01 -0400

    pipeline groovy

---
+++ vars/terraform.groovy
@@ -0,0 +1,23 @@
+def call(Map config = [:]){
+    pipeline{
+    agent any 
+    stages {
+        stage("deploy terraform"){
+            steps{
+                withCredentials([[
+                    $class: 'AmazonWebServicesCredentialsBinding',
+                    credentialsId: "jenkins-terraform",
+                    accessKeyVariable: "AWS_ACCESS_KEY_ID",
+                    secretKeyVariable: "AWS_SECRET_KEY_ID"
+
+                ]]){
+                    sh '/usr/local/bin/terraform init -backend-config=${config.access_key}=${AWS_ACCESS_KEY_ID} -backend-config=${config.secret_key}=${AWS_SECRET_KEY_ID}'
+                    sh '/usr/local/bin/terraform plan -var ${config.access_key}=${AWS_ACCESS_KEY_ID} -var ${config.secret_key}=${AWS_SECRET_KEY_ID} -out Outputforplan'
+                    sh '/usr/local/bin/terraform apply -input=false Outputforplan'
+                }
+            }
+        }
+
+    }
+}
+}
