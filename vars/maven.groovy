5a835a45ebd2e07ccd65565ebb0c2c2b8888e8ac
venu <venugopal.k1994@gmail.com>
2021-08-25 14:43:15 -0400

    mvn

---
+++ vars/maven.groovy
@@ -0,0 +1,21 @@
+def call(){
+    pipeline{
+    agent any
+    stages{   
+        stage("testing maven"){
+            steps{
+                
+                sh 'mvn test'
+                
+            }
+        }
+        stage("maven package"){
+            steps{
+                
+                sh 'mvn package'
+                
+            }
+        }
+    }
+}
+}
